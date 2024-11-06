da67c2df1e0ba4e50bde2f4a72b8ca603ea6bac7
MSLI <lijiayuan20212021@163.com>
2024-11-06 10:13:25 +0800

    苍穹外卖初始代码

--- sky-server/src/main/java/com/sky/SkyApplication.java
+++ sky-server/src/main/java/com/sky/SkyApplication.java
@@ -1,6 +1,8 @@
 package com.sky;
 
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
